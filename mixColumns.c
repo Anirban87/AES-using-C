80fae7c493ddddee9c00b4902a4fd339694bf83f
Anirban Mukherjee <Anirban87@users.noreply.github.com>
2017-07-02 03:07:06 +0530

    mix colums function

--- mixColumns.c
+++ mixColumns.c
@@ -10,5 +10,6 @@ void MixColumns()					// MixColumns function mixes the columns of the state matr
 		Tm = state[1][i] ^ state[2][i] ; Tm = xtime(Tm); state[1][i] ^= Tm ^ Tmp ;
 		Tm = state[2][i] ^ state[3][i] ; Tm = xtime(Tm); state[2][i] ^= Tm ^ Tmp ;
 		Tm = state[3][i] ^ t ; Tm = xtime(Tm); state[3][i] ^= Tm ^ Tmp ;
+		
 	}
 }
